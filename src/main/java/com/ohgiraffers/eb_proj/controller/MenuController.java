73512965e4a69420ec9fbc93dc3c9554c593c13e
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-30 12:25:38 +0900

    add 123

--- src/main/java/com/ohgiraffers/eb_proj/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/eb_proj/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "eb_proj is fine!!";
+        return "eb_proj is fine!!123";
     }
 
     @GetMapping("menus/{menuCode}")
